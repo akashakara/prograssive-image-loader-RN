19c985bf9bbadda847f7a8b18e91e6d1391be300
Akash Kumar Singh <akash.akara@gmail.com>
2020-09-04 12:55:30 +0530

    facebook-style-image-loader C10

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import ProgressiveImage from './ProgressiveImage';
+
+const w = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ProgressiveImage
+          thumbnailSource={{ uri: `https://scontent.fpat1-1.fna.fbcdn.net/v/t1.0-9/67782063_1407586609382016_1464967693458210816_o.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=yRz96LQ4sTwAX9E6V_N&_nc_ht=scontent.fpat1-1.fna&oh=b8d5c0bbc33494ba6b4cb39948fa1cf9&oe=5F796902` }}
+          source={{ uri: `https://scontent.fpat1-1.fna.fbcdn.net/v/t1.0-9/67782063_1407586609382016_1464967693458210816_o.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=yRz96LQ4sTwAX9E6V_N&_nc_ht=scontent.fpat1-1.fna&oh=b8d5c0bbc33494ba6b4cb39948fa1cf9&oe=5F796902` }}
+          style={{ width: w.width, height: w.width }}
+          resizeMode="cover"
+        />
+      </View>
+    );
+  }
+}
